e3a80a3e755564c20e5c32005c852ef960992d3b
Godand <aezeigwe2003@gmail.com>
2024-08-08 17:47:23 +0200

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        const string script = @"
+  window.addEventListener('message', (event) => {
+    const data = event.data;
+    window.webkit.messageHandlers.nativeApp.postMessage(data);
+  });
+";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            string text = "This is a Toast";
+            ToastDuration duration = ToastDuration.Short;
+            double fontSize = 14;
+
+            var toast = Toast.Make(text, duration, fontSize);
+
+            await toast.Show(cancellationTokenSource.Token);
+        }
+
+        private async Task InjectJSEventListener()
+        {
+            await TWebView.EvaluateJavaScriptAsync(script);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            TWebView.Reload();
+            
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            if( TWebView.CanGoForward)
+                TWebView.GoForward();
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            if( TWebView.CanGoBack)
+                TWebView.GoBack();
+        }
+    }
+
+}
